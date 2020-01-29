4620f797ca1a826fb8bfb20f05293e2305ce7fa0
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-01-29 17:04:12 +0530

    Add routes and route resolution

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+// load up our shiny new route for users
+const userRoutes = require('./users');
+
+const appRouter = (app, fs) => {
+    // we've added in a default route here that handles empty routes
+    // at the base API url
+    app.get('/', (req, res) => {
+        res.send('welcome to the development api-server');
+    });
+
+    // run our user route module here to complete the wire up
+    userRoutes(app, fs);
+};
+
+module.exports = appRouter;
